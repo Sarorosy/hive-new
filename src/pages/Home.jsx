defdb118ed65628935b72fe1dbb22343acd3d36f
sarorosy <sn11std@gmail.com>
2025-12-04 16:20:08 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
         {/* <SmallAbout /> */}
         <FeaturedCentres />
         <Members />
-        <CommunityExperience />
+        {/* <CommunityExperience /> */}
         {/* <HeroVideo /> */}
         {/* <AmenitiesSection/> */}
         <LatestBlogs/>
